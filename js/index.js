f0076489ea0fc857d9853266e2175f1c04300c32
Simon Bourne <simonbourne@gmail.com>
2021-03-24 22:33:27 +0000

    Hello, UI5 world!

---
+++ js/index.js
@@ -0,0 +1,2 @@
+import "@ui5/webcomponents/dist/Button";
+import("../pkg/index.js").catch(console.error);
---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+use moxie_dom::{
+    elements::html::{div, p},
+    html_element,
+    interfaces::node::Parent,
+    prelude::{state, document},
+};
+use wasm_bindgen::prelude::wasm_bindgen;
+
+html_element! {
+    <ui5-button>
+    categories { Flow }
+
+    children {
+        categories {
+            Flow
+        }
+    }
+}
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    moxie_dom::boot(document().body(), || {
+        let (expand_count, inc_on_expand) = state(|| 0);
+        let (selected_count, inc_on_select) = state(|| 0);
+
+        div().child(ui5_button().child("Hello, world!"))
+    });
+
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    console_error_panic_hook::set_once();
+
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
