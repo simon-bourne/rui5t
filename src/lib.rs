47cfb4f6ab84404fcec1a913de67c70d16afa691
Simon Bourne <simonbourne@gmail.com>
2021-03-24 22:44:00 +0000

    Setup panic hooks before moxie boot

--- src/lib.rs
+++ src/lib.rs
@@ -22,20 +22,23 @@ html_element! {
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
+    // This provides better error messages in debug mode.
+    // It's disabled in release mode so it doesn't bloat up the file size.
+    #[cfg(debug_assertions)]
+    {
+        console_error_panic_hook::set_once();
+        console::log_1(&JsValue::from_str("Debug"));
+    }
+
+    #[cfg(not(debug_assertions))]
+    console::log_1(&JsValue::from_str("Release"));
+
     moxie_dom::boot(document().body(), || {
         let (expand_count, inc_on_expand) = state(|| 0);
         let (selected_count, inc_on_select) = state(|| 0);
 
         div().child(ui5_button().child("Hello, world!"))
     });
-
-    // This provides better error messages in debug mode.
-    // It's disabled in release mode so it doesn't bloat up the file size.
-    #[cfg(debug_assertions)]
-    console_error_panic_hook::set_once();
-
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
 
     Ok(())
 }
